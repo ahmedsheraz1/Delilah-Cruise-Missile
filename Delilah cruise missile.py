fc133e3e3962d38f160994c02e2e81ae33be30d0
Ahmed Sheraz <ahmedsheraz1@gmail.com>
2022-12-14 02:34:42 +0500

    Create Delilah cruise missile.py

---
+++ Delilah cruise missile.py
@@ -0,0 +1,5 @@
+Python 3.10.2 (v3.10.2:a58ebcc701, Jan 13 2022, 14:50:16) [Clang 13.0.0 (clang-1300.0.29.30)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+from hashlib import blake2b
+blake2b(b'Delilah cruise missile').hexdigest()
+'67e2fa9944d7d6025439d9a296853b1fb737a665aede3b25894d476fefd724e58130158e6d990db7e794cafe517c123f3e547322fcc03edbf349e4417b22f101'
